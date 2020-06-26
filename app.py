9c38823dd166c99f73ef0476c86fcb9d34be26e3
AMAN VARYANI <30382575+amanv1906@users.noreply.github.com>
2020-06-26 10:27:53 +0530

    First commit

---
+++ app.py
@@ -0,0 +1,32 @@
+import streamlit as stm
+from tensorflow.keras.models import load_model
+import numpy as np 
+import pandas as pd
+import nltk
+nltk.download('punkt')
+from nltk import word_tokenize
+stm.title('Smart Predictor')
+import tensorflow as tf
+import os
+from tensorflow.python.keras.layers import Layer
+from tensorflow.python.keras import backend as K
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import pickle
+import time
+import tensorflow as tf
+from tensorflow.python.keras.layers import Layer
+from tensorflow.python.keras import backend as K
+def main():
+    stm.text("Hello")
+
+
+if __name__ == '__main__':
+	main()
+
+
+
+
+
+
+
+    
